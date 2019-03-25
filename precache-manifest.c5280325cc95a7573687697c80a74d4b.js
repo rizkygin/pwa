d8c7d0b03868ccc5fd5b645a6bf517014137380c
rizkygin <rizkygin@gmail.com>
2019-03-25 11:39:20 +0700

    Updates

---
+++ precache-manifest.c5280325cc95a7573687697c80a74d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/baru/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "a6738b61776bee4fd2b7",
+    "url": "/baru/static/js/runtime~main.0ced07a0.js"
+  },
+  {
+    "revision": "86e11186a138a592c4d8",
+    "url": "/baru/static/js/main.4997be3f.chunk.js"
+  },
+  {
+    "revision": "3c688a83c6a96b1bd515",
+    "url": "/baru/static/js/2.f885ce20.chunk.js"
+  },
+  {
+    "revision": "86e11186a138a592c4d8",
+    "url": "/baru/static/css/main.5f9effcb.chunk.css"
+  },
+  {
+    "revision": "3e4bb9dbe2eb188b4aa59daf25fa80ef",
+    "url": "/baru/index.html"
+  }
+];
